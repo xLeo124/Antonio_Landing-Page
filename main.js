8db227b12a3760ee636c0e6b33039ea3b8da6bec
xLeo124 <manuelleoantonio0403@gmail.com>
2024-04-29 16:22:47 +0800

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        nav: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    });
+});
+
